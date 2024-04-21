1d1fc1a37c6ed2e7be212151ed2395fcf8392399
SohaNathani <snathan6@students.kennesaw.edu>
2024-04-20 22:02:29 -0400

    Added nutrition info for lunch

--- Nutrition fitness pro main code.py
+++ Nutrition fitness pro main code.py
@@ -3,6 +3,11 @@ print('Welcome to Fitness Pro, a nutrition guide where you will be choosing from
 def nutrition_facts(recipe, portion_size):
     # Nutrition facts for each recipe
     nutrition_data = {
-        Veggie and Cheese Omelette_b: {"calories": 300, "carbs": 10, "protein": 20, "fat": 15},
-        Protein Pancakes_b: {"calories": 250, "carbs": 30, "protein": 20, "fat": 8},
-        Chia Seed Pudding_b: {"calories": 180, "carbs": 20, "protein": 5, "fat": 10},
+        Veggie and Cheese Omelette_B: {"calories": 300, "carbs": 10, "protein": 20, "fat": 15},
+        Protein Pancakes_B: {"calories": 250, "carbs": 30, "protein": 20, "fat": 8},
+        Chia Seed Pudding_B: {"calories": 180, "carbs": 20, "protein": 5, "fat": 10},
+
+        Chicken & Tzatziki Wraps_L: {"calories": 400, "carbs": 35, "protein": 30, "fat": 18},
+        Pesto Spinach Penne_L: {"calories": 350, "carbs": 45, "protein": 12, "fat": 14},
+        Veggie Bacon Club Sandwich_L: {"calories": 450, "carbs": 40, "protein": 15, "fat": 25},
+        Chicken, Spinach & Feta Wrap_L: {"calories": 380, "carbs": 30, "protein": 25, "fat": 20},
