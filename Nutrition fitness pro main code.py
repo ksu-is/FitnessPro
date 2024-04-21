aa2e1648babc327febe4e59f8ac14220b4524051
SohaNathani <snathan6@students.kennesaw.edu>
2024-04-20 21:55:39 -0400

    Started coding by adding nutrition info for breakfast recepies

--- Nutrition fitness pro main code.py
+++ Nutrition fitness pro main code.py
@@ -1,1 +1,8 @@
 print('Welcome to Fitness Pro, a nutrition guide where you will be choosing from pre selected meals for breakfast, lunch, dinner,and snacks.')
+
+def nutrition_facts(recipe, portion_size):
+    # Nutrition facts for each recipe
+    nutrition_data = {
+        Veggie and Cheese Omelette_b: {"calories": 300, "carbs": 10, "protein": 20, "fat": 15},
+        Protein Pancakes_b: {"calories": 250, "carbs": 30, "protein": 20, "fat": 8},
+        Chia Seed Pudding_b: {"calories": 180, "carbs": 20, "protein": 5, "fat": 10},
