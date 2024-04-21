87f3b0e27e759d00f5aefab8b3bbb48e0e7e294e
SohaNathani <snathan6@students.kennesaw.edu>
2024-04-20 22:06:15 -0400

    added the last nutrition info part of snacks

--- Nutrition fitness pro main code.py
+++ Nutrition fitness pro main code.py
@@ -15,3 +15,8 @@ def nutrition_facts(recipe, portion_size):
         Sheet-Pan Salmon with Crispy Quinoa_D: {"calories": 420, "carbs": 30, "protein": 35, "fat": 18},
         Chickpea Pasta with Mushrooms & Kale_D: {"calories": 350, "carbs": 40, "protein": 15, "fat": 12},
         Chicken Tikka Masala with Rice_D: {"calories": 400, "carbs": 45, "protein": 25, "fat": 16},
+
+        Chicken Caesar Salad_S: {"calories": 320, "carbs": 15, "protein": 30, "fat": 18},
+        Apple & Peanut Butter Toast_S: {"calories": 200, "carbs": 25, "protein": 5, "fat": 10},
+        Acai Bowl_S: {"calories": 280, "carbs": 40, "protein": 8, "fat": 12}
+    }
